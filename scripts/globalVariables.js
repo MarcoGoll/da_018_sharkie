ad07cf49b965849abc1cfc24161246e8d6e0150f
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2024-12-20 17:06:47 +0100

    improve sounds

--- scripts/globalVariables.js
+++ scripts/globalVariables.js
@@ -7,10 +7,10 @@ const AMOUNTBACKGROUNDS = 5;
 poisonSound = new Audio('./assets/audio/poison.mp3');
 poisonSound.volume = 0.4;
 
-coinSound = new Audio('./assets/audio/coin4.mp3');
-coinSound.volume = 0.5;
-coinSound.playbackRate = 1.0
+coinSound = new Audio('./assets/audio/coin.mp3');
+coinSound.volume = 1;
+coinSound.playbackRate = 1.0;
 
 
 shootBubbleSound = new Audio('./assets/audio/shootBubble.mp3');
-coinSound.volume = 0.1;
+shootBubbleSound.volume = 0.1;
