b81efebc76503f306c7845cde97ca06d00db210d
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-01-03 12:32:53 +0100

    refactor: clean code fish classes)

--- models/jellyfish.class.js
+++ models/jellyfish.class.js
@@ -3,20 +3,17 @@ class Jellyfish extends MoveableObject {
     y;
     width;
     height;
-
-    isMovingUp = false;
-    isMovingDown = true;
-    iDead = 0;
-
     offset = {
         x: 30,
         y: 30,
         widht: 60,
         height: 70,
     }
+    isMovingUp = false;
+    isMovingDown = true;
+    iDead = 0;
     energyMax = 200;
     energy = 200;
-
     isHitAudio = new Audio('./assets/audio/enemyIsHit.mp3')
     isDyingAudio = new Audio('./assets/audio/jellyfishIsDying.mp3')
 
@@ -28,52 +25,79 @@ class Jellyfish extends MoveableObject {
         this.height = height;
         this.isHitAudio.volume = 0.5;
         this.isDyingAudio.volume = 0.5;
-        this.setSounds();
+        this.setSoundsMuted();
         this.animate();
     }
 
     animate() {
-        addStoppableIntervallId(setInterval(() => {
-            //this.moveLeft();   
-
-            if (this.y < 300 && this.isMovingDown) {
-                this.moveDown();
-                this.isMovingDown = true;
-                if (this.y > 300) {
-                    this.isMovingDown = false;
-                    this.isMovingUp = true;
-                }
-            }
-
-            if (this.y > 0 && this.isMovingUp) {
-                this.moveUp();
-                this.isMovingUp = true;
-                if (this.y < 0) {
-                    this.isMovingUp = false;
-                    this.isMovingDown = true;
-                }
-            }
-        }, 1000 / 60)); // 60fps
-
-        addStoppableIntervallId(setInterval(() => {
-            if (this.isDead()) {
-                if (this.iDead < this.IMAGES_DEAD.length) {
-                    if (this.deadAnimationWasPlayed == false) {
-                        this.currentImage = 0;
-                        this.deadAnimationWasPlayed = true;
-                    }
-                    this.playAnimation(this.IMAGES_DEAD);
-                    this.iDead++
-                    console.log(this.iDead);
-                }
-            }
-            else {
-                this.playAnimation(this.IMAGES_SWIM);
-            }
-        }, 1000 / 6));
+        addStoppableIntervallId(setInterval(() => this.moveJellyfish(), 1000 / 60)); // 60fps
+        addStoppableIntervallId(setInterval(() => this.playJellyfishAnimations(), 1000 / 6));
     }
 
-    setSounds() {
+    playJellyfishAnimations() {
+        if (this.isDead()) this.playDeadAnimation();
+        else this.playMoveAnimation();
+    }
+
+    playDeadAnimation() {
+        if (this.isDeadAnimationStillRequired()) {
+            if (this.deadAnimationWasPlayed == false) {
+                this.currentImage = 0;
+                this.deadAnimationWasPlayed = true;
+            }
+            this.playAnimation(this.IMAGES_DEAD);
+            this.iDead++
+            console.log(this.iDead);
+        }
+    }
+
+    playMoveAnimation() {
+        this.playAnimation(this.IMAGES_SWIM);
+    }
+
+    moveJellyfish() {
+        if (this.shouldMoveDown()) {
+            this.moveDown();
+        }
+        if (this.shouldMoveUp()) {
+            this.moveUp();
+        }
+    }
+
+    moveDown() {
+        super.moveDown();
+        this.isMovingDown = true;
+        if (this.y > 300) {
+            this.isMovingDown = false;
+            this.isMovingUp = true;
+        }
+    }
+
+    moveUp() {
+        super.moveUp();
+        this.isMovingUp = true;
+        if (this.y < 0) {
+            this.isMovingUp = false;
+            this.isMovingDown = true;
+        }
+    }
+
+    shouldMoveDown() {
+        return this.y < 300 && this.isMovingDown;
+    }
+
+    shouldMoveUp() {
+        return this.y > 0 && this.isMovingUp;
+    }
+
+    isDeadAnimationStillRequired() {
+        return this.iDead < this.IMAGES_DEAD.length;
+    }
+
+    /**
+  * set muted attribut of global sounds
+  */
+    setSoundsMuted() {
         if (muteMode) {
             this.isHitAudio.muted = true;
             this.isDyingAudio.muted = true;
--- models/pufferfish.class.js
+++ models/pufferfish.class.js
@@ -3,23 +3,21 @@ class Pufferfish extends MoveableObject {
     y;
     width;
     height;
-
-    energyMax = 200;
-    energy = 200;
-
-    transitionAnimationWasPlayed = false;
-    deadAnimationWasPlayed = false;
-    iDead = 0;
-
     offset = {
         x: 10,
         y: 10,
         widht: 30,
         height: 30,
     }
+    energyMax = 200;
+    energy = 200;
+    transitionAnimationWasPlayed = false;
+    deadAnimationWasPlayed = false;
+    iDead = 0;
+    isHitAudio = new Audio('./assets/audio/enemyIsHit.mp3');
+    isDyingAudio = new Audio('./assets/audio/pufferfishIsDying.mp3');
+    iTransition = 0;
 
-    isHitAudio = new Audio('./assets/audio/enemyIsHit.mp3')
-    isDyingAudio = new Audio('./assets/audio/pufferfishIsDying.mp3')
 
     constructor(x, y, width, height) {
         super();
@@ -29,52 +27,72 @@ class Pufferfish extends MoveableObject {
         this.height = height;
         this.isHitAudio.volume = 0.5;
         this.isDyingAudio.volume = 0.5;
-        this.setSounds();
+        this.setSoundsMuted();
         this.animate();
     }
 
     animate() {
-        let iTransition = 0;
-
-
-        addStoppableIntervallId(setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60)); // 60fps
-
-        addStoppableIntervallId(setInterval(() => {
-
-            if (this.energy < this.energyMax && !(this.isDead())) { //Transition Animation
-                if (iTransition < this.IMAGES_TRANSITION.length) {
-                    if (this.transitionAnimationWasPlayed == false) {
-                        this.currentImage = 0;
-                        this.transitionAnimationWasPlayed = true;
-                    }
-                    this.playAnimation(this.IMAGES_TRANSITION);
-                    iTransition++
-                }
-                else {
-                    this.playAnimation(this.IMAGES_AGRESSIVESWIM);
-                }
-            } else if (this.isDead()) {
-                if (this.iDead < this.IMAGES_DEAD.length) {
-                    if (this.deadAnimationWasPlayed == false) {
-                        this.currentImage = 0;
-                        this.deadAnimationWasPlayed = true;
-                    }
-                    this.playAnimation(this.IMAGES_DEAD);
-                    this.iDead++
-                    console.log(this.iDead);
-                }
-            }
-            else {
-                this.playAnimation(this.IMAGES_SWIM);
-            }
-
-
-        }, 1000 / 10));
+        addStoppableIntervallId(setInterval(() => this.movePufferfish(), 1000 / 60)); // 60fps
+        addStoppableIntervallId(setInterval(() => this.playPufferfishAnimations(), 1000 / 10));
     }
 
-    setSounds() {
+    movePufferfish() {
+        this.moveLeft();
+    }
+
+    playPufferfishAnimations() {
+        if (this.isHurt()) {
+            if (this.isTransitionAnimationStillRequired()) this.playTransitionAnimation();
+            else this.playMoveAgressiveAnimation();
+        } else if (this.isDead()) {
+            if (this.isDeadAnimationStillRequired()) this.playDeadAnimation();
+        }
+        else this.playMoveNormalAnimation();
+    }
+
+    playTransitionAnimation() {
+        if (this.transitionAnimationWasPlayed == false) {
+            this.currentImage = 0;
+            this.transitionAnimationWasPlayed = true;
+        }
+        this.playAnimation(this.IMAGES_TRANSITION);
+        this.iTransition++
+    }
+
+    playMoveAgressiveAnimation() {
+        this.playAnimation(this.IMAGES_AGRESSIVESWIM); //MoveAGRESSIVE 
+    }
+
+    playDeadAnimation() {
+        if (this.deadAnimationWasPlayed == false) {
+            this.currentImage = 0;
+            this.deadAnimationWasPlayed = true;
+        }
+        this.playAnimation(this.IMAGES_DEAD);
+        this.iDead++
+        console.log(this.iDead);
+    }
+
+    playMoveNormalAnimation() {
+        this.playAnimation(this.IMAGES_SWIM); //MoveNormal
+    }
+
+    isHurt() {
+        return this.energy < this.energyMax && !(this.isDead());
+    }
+
+    isTransitionAnimationStillRequired() {
+        return this.iTransition < this.IMAGES_TRANSITION.length;
+    }
+
+    isDeadAnimationStillRequired() {
+        return this.iDead < this.IMAGES_DEAD.length;
+    }
+
+    /**
+   * set muted attribut of global sounds
+   */
+    setSoundsMuted() {
         if (muteMode) {
             this.isHitAudio.muted = true;
             this.isDyingAudio.muted = true;
